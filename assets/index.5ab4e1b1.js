150af61fda1a0c7b491bee44e81d102e41259f95
uncortvpro <90911508+uncortvpro@users.noreply.github.com>
2023-11-13 09:21:26 +0000

    Deploying to gh-pages from @ ukrainemart/ukrainemart-client@a3b58ef7526a0de96cf460d5ff6c919d835aaf1c 🚀

---
+++ assets/index.5ab4e1b1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.0825faa6.js";import{X as r,M as c}from"./swiper-vue.c72e2a3a.js";const n={};function o(t,s){return c(),r("div",null,"Особисті дані")}const f=e(n,[["render",o]]);export{f as default};
