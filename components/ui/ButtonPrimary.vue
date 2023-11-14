71e1f4247722b390346cafe4e2351afd639a5291
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-11-14 18:51:18 +0100

    add new pages

--- components/ui/ButtonPrimary.vue
+++ components/ui/ButtonPrimary.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
   type Props = {
     styles?: string; // Additional custom styles for the button
+    to?: string;
   };
 
   withDefaults(defineProps<Props>(), {
     styles: '',
+    to: '',
   });
+
+  const defaultStyles =
+    'group inline-flex items-center justify-center gap-[9px] rounded-[50px] border border-solid border-black bg-black px-[27px] py-[7px] font-montserrat text-[12px] font-medium leading-4 text-white transition-colors duration-100 ease-in hover:bg-transparent hover:text-black focus:outline-none disabled:cursor-not-allowed md:px-[33px] md:py-[9px] md:text-[16px] md:leading-[19px] 4xl:border-[2px] 4xl:px-[38px] 4xl:py-[11px] 4xl:text-[20px] 4xl:leading-6';
 </script>
 
 <template>
-  <button
-    type="button"
-    :class="
-      cn(
-        'group flex items-center justify-center gap-[9px] rounded-[50px] border border-solid border-black bg-black px-[27px] py-[7px] font-montserrat text-[12px] font-medium leading-4 text-white transition-colors duration-100 ease-in hover:bg-transparent hover:text-black focus:outline-none disabled:cursor-not-allowed md:px-[33px] md:py-[9px] md:text-[16px] md:leading-[19px] 4xl:border-[2px] 4xl:px-[38px] 4xl:py-[11px] 4xl:text-[20px] 4xl:leading-6',
-        styles
-      )
-    "
-  >
+  <NuxtLink v-if="to" :to="to" type="button" :class="cn(defaultStyles, styles)">
+    <slot />
+  </NuxtLink>
+
+  <button v-else type="button" :class="cn(defaultStyles, styles)">
     <slot />
   </button>
 </template>
---
+++ pages/profile/index/add_product.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <LayoutProfilePage title="Додати товари">
+    <!--  -->
+  </LayoutProfilePage>
+</template>
+
+<style></style>
