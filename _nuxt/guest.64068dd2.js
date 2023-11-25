a92ef788bde0d1df0a57297a8f69f4867ad35d0d
bohdanyezhlov <bohdanyezhlov@gmail.com>
2023-11-25 09:01:53 +0000

    Deploying to gh-pages from @ ukrainemart/ukrainemart-client@f4733f753e12ca7efdd06afbe37cea929377ea6a 🚀

---
+++ _nuxt/guest.64068dd2.js
@@ -0,0 +1,1 @@
+import{k as e,g as t,n as r}from"./entry.08e485ea.js";import"./swiper-vue.33fa7a2f.js";const s=e(()=>t().isLoggedIn?r("/profile",{replace:!0}):!0);export{s as default};
---
+++ _nuxt/tw-merge.9c6d0b77.js
@@ -0,0 +1,1 @@
+import{B as e,C as a}from"./entry.08e485ea.js";var t=e(a);export{t};
