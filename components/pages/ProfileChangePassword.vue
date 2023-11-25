6dbb8d33d9125a5990fa49d079eec4317f3050a4
Roman <romazelenskiy2000@gmail.com>
2023-11-25 11:34:53 +0200

    add modal warning

--- components/pages/ProfileChangePassword.vue
+++ components/pages/ProfileChangePassword.vue
@@ -2,14 +2,41 @@
   const auth = useAuthStore();
   const isPassword = computed(() => auth.isPassword);
 
+  const inputs = ref({
+    oldPassword: '',
+    newPassword: '',
+    newConfPassword: '',
+  });
+
   const changePassword = () => {
     useApi(`${useUrlApi()}/user/password/change`, {
+      method: 'POST',
       body: {
-        old_password: 'required|min:6|max:20',
-        new_password: 'required|min:6|max:20',
-        new_c_password: 'required|min:6|max:20|same:new_password',
+        old_password: inputs.value.oldPassword,
+        new_password: inputs.value.newPassword,
+        new_c_password: inputs.value.newConfPassword,
+      },
+    }).then((res) => {
+      console.log(res);
+    });
+  };
+
+  const setPassword = () => {
+    useApi(`${useUrlApi()}/user/password/set`, {
+      method: 'POST',
+      body: {
+        new_password: inputs.value.newPassword,
+        new_c_password: inputs.value.newConfPassword,
       },
     });
+  };
+
+  const onSetPassword = () => {
+    if (isPassword) {
+      changePassword();
+      return false;
+    }
+    setPassword();
   };
 </script>
 
@@ -18,19 +45,19 @@
     class="mt-[30px] flex flex-col gap-y-[15px] sm:gap-x-[50px] md:mt-[35px] md:gap-x-[84px] md:gap-y-[20px] xl:mt-[55px] 2xl:gap-x-[150px] 2xl:gap-y-[25px]"
   >
     <UiLabel v-if="isPassword" class="row-span-1" :label="$t('password_change.old_password') + ':'">
-      <UiInputOutline />
+      <UiInputOutlinePassword v-model="inputs.oldPassword" />
     </UiLabel>
     <UiLabel
       class="row-span-1"
       :label="(!isPassword ? $t('password') : $t('password_change.new_password')) + ':'"
     >
-      <UiInputOutline />
+      <UiInputOutlinePassword v-model="inputs.newPassword" />
     </UiLabel>
     <UiLabel class="row-span-1" :label="$t('repeatPassword') + ':'">
-      <UiInputOutline />
+      <UiInputOutlinePassword v-model="inputs.newConfPassword" />
     </UiLabel>
     <div class="col-span-2 flex justify-center">
-      <UiButtonPrimary class="w-fit">{{
+      <UiButtonPrimary class="w-fit" @click="onSetPassword">{{
         !isPassword ? $t('password_change.set_password') : $t('change_password')
       }}</UiButtonPrimary>
     </div>
---
+++ components/ui/ModalWarning.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+  defineProps<{
+    ui?: object;
+  }>();
+
+  const emits = defineEmits(['closeModal', 'confirm']);
+
+  const closeModal = () => {
+    emits('closeModal');
+  };
+
+  const confirm = () => {
+    emits('confirm');
+  };
+</script>
+
+<template>
+  <UiModal :ui="{ ...ui, width: '!max-w-[414px]' }">
+    <div class="flex flex-col items-center">
+      <div
+        class="flex h-[20px] w-[20px] items-center justify-center rounded-[50%] border border-status_red text-[15px] text-status_red md:h-[30px] md:w-[30px]"
+      >
+        <span>!</span>
+      </div>
+      <p class="mt-[15px] text-center text-[15px] font-medium md:mt-[15px] md:text-[20px]">
+        <slot />
+      </p>
+      <div class="mt-[20px] flex justify-center gap-[20px] self-stretch">
+        <UiButtonOutline @click="confirm">Так</UiButtonOutline>
+        <UiButtonOutline
+          class="w-full max-w-[115px] bg-black text-white hover:bg-transparent hover:text-black hover:!opacity-100"
+          @click="closeModal"
+          >Ні
+        </UiButtonOutline>
+      </div>
+    </div>
+  </UiModal>
+</template>
+
+<style scoped></style>
