adcbc5a4e1a8f0151424b9bd278c7ab77b0c949a
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-12-07 17:02:52 +0100

    add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,6 +68,12 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Ukrainemart',
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, maximum-scale=1',
+        },
+      ],
     },
   },
 });
