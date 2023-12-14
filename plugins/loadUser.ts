25aeaa018d909f7a40294260c492df434573f867
Roman <romazelenskiy2000@gmail.com>
2023-12-14 23:13:59 +0200

    fix

--- plugins/loadUser.ts
+++ plugins/loadUser.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin(async () => {
   const auth = useAuthStore();
   const token = useCookie('XSRF-TOKEN');
 
-  if (token.value) {
+  if (!token.value) {
     await useApiFetch(`${useUrl()}/sanctum/csrf-cookie`);
   }
 
