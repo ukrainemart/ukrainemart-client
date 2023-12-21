c7ddb0f52c80a60768297654c0ab6e02a1dd24fe
Roman <romazelenskiy2000@gmail.com>
2023-12-21 23:42:22 +0200

    fix

--- plugins/loadUser.ts
+++ plugins/loadUser.ts
@@ -2,18 +2,8 @@ export default defineNuxtPlugin(async () => {
   const auth = useAuthStore();
   const token = useCookie('XSRF-TOKEN');
 
-  function deleteCookie(name: any, domain: any) {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${domain};`;
-  }
-
-  // Remove the unwanted XSRF-TOKEN cookie for ukrainemart.com
-  if (process.client) {
-    if (!token.value) {
-      await useApiFetch(`${useUrl()}/sanctum/csrf-cookie`);
-    }
-    setTimeout(() => {
-      deleteCookie('XSRF-TOKEN', 'ukrainemart.com');
-    }, 5000);
+  if (!token.value) {
+    await useApiFetch(`${useUrl()}/sanctum/csrf-cookie`);
   }
 
   if (!auth.isLoggedIn) {
