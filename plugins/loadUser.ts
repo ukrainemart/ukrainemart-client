f0630aa41954c9ae1d49328b45f0b44fd447db35
Roman <romazelenskiy2000@gmail.com>
2023-12-21 22:48:38 +0200

    fix

--- plugins/loadUser.ts
+++ plugins/loadUser.ts
@@ -1,17 +1,15 @@
 export default defineNuxtPlugin(async () => {
   const auth = useAuthStore();
   const token = useCookie('XSRF-TOKEN');
+  const tokenRemove = useCookie('XSRF-TOKEN', {
+    domain: 'ukrainemart.com',
+  });
 
   if (!token.value) {
     await useApiFetch(`${useUrl()}/sanctum/csrf-cookie`);
   }
 
-  function deleteCookie(name, domain) {
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${domain};`;
-  }
-
-  // Remove the unwanted XSRF-TOKEN cookie for ukrainemart.com
-  deleteCookie('XSRF-TOKEN', 'ukrainemart.com');
+  tokenRemove.value = '';
 
   if (!auth.isLoggedIn) {
     await auth.fetchUser();
--- plugins/test.ts
+++ plugins/test.ts
@@ -1,11 +1,11 @@
 export default defineNuxtPlugin(async (app) => {
-  // let defaultLanguage = 'en';
-  // if (context.req.headers['accept-language']) {
-  //   const browserLanguage = context.req.headers['accept-language'].split(',')[0];
-  //   if (browserLanguage.startsWith('uk')) {
-  //     defaultLanguage = 'uk';
-  //   }
-  // }
+  let defaultLanguage = 'en';
+  if (context.req.headers['accept-language']) {
+    const browserLanguage = context.req.headers['accept-language'].split(',')[0];
+    if (browserLanguage.startsWith('uk')) {
+      defaultLanguage = 'uk';
+    }
+  }
 
-  // context.app.i18n.locale = defaultLanguage;
+  context.app.i18n.locale = defaultLanguage;
 });
