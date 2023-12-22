2e9e2b0007272b7c0727fb807fe818cf5654431a
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-12-22 19:06:47 +0100

    rename SkeletonRequestImporter to SkeletonRequest

--- components/common/SectionRequestsImporters.vue
+++ components/common/SectionRequestsImporters.vue
@@ -17,13 +17,14 @@
 <template>
   <div v-if="loading" class="container mt-[50px] xl:mt-[70px]">
     <UiSkeleton class="mb-[40px] h-[20px] w-[30%] md:h-[30px]" />
+
     <div
       class="mb-[30px] grid grid-cols-12 gap-x-[25px] gap-y-5 md:mb-[40px] md:gap-x-[32px] md:gap-y-10 lg:mb-[50px] 4xl:gap-x-[44px] 4xl:gap-y-[60px]"
     >
-      <SkeletonRequestImporter class="col-span-12 sm:!col-span-6 md:!col-span-4" />
-      <SkeletonRequestImporter class="col-span-6 hidden sm:!col-span-6 sm:block md:!col-span-4" />
+      <SkeletonRequest class="col-span-12 sm:!col-span-6 md:!col-span-4" />
+      <SkeletonRequest class="col-span-6 hidden sm:!col-span-6 sm:block md:!col-span-4" />
 
-      <SkeletonRequestImporter class="col-span-6 hidden sm:!col-span-6 md:!col-span-4 md:block" />
+      <SkeletonRequest class="col-span-6 hidden sm:!col-span-6 md:!col-span-4 md:block" />
     </div>
   </div>
   <UiSectionTitleSlider
--- components/skeleton/MyRequest.vue
+++ components/skeleton/MyRequest.vue
@@ -5,23 +5,24 @@
     class="col-span-12 grid grid-cols-2 rounded-[20px] bg-status_dark_gray px-[20px] py-[15px] md:col-span-6 md:p-[30px] xl:col-span-4"
   >
     <div class="col-span-1 flex items-center gap-[10px]">
-      <UiSkeleton
-        class="h-[16px] w-[21px] md:h-[23px] md:w-[29px] 4xl:h-[30px] 4xl:w-[36px]"
-        src="@img/flag_spain.png"
-        alt="flag_spain"
-      />
+      <UiSkeleton class="h-[16px] w-[21px] md:h-[23px] md:w-[29px] 4xl:h-[30px] 4xl:w-[36px]" />
       <UiSkeleton class="h-[10px] w-[60px] md:h-[15px] 4xl:h-[18px]" />
     </div>
+
     <div class="relative z-20 col-span-1 flex justify-end gap-[7px]">
-      <UiSkeleton class="h-[30px] w-[30px] rounded-[50%] md:h-[40px] md:w-[40px]" />
-      <UiSkeleton class="h-[30px] w-[30px] rounded-[50%] md:h-[40px] md:w-[40px]" />
+      <UiSkeleton
+        class="h-[25px] w-[25px] rounded-[50%] md:h-[30px] md:w-[30px] lg:h-[40px] lg:w-[40px]"
+      />
+      <UiSkeleton
+        class="h-[25px] w-[25px] rounded-[50%] md:h-[30px] md:w-[30px] lg:h-[40px] lg:w-[40px]"
+      />
     </div>
-    <h3
-      class="col-span-2 mt-[5px] text-[17px] font-medium md:mt-[12px] md:text-[20px] 4xl:mt-[21px]"
-    />
-    <UiTextPortalPrimary class="col-span-2 mt-[10px] text-status_gray 4xl:mt-[15px]" />
+
+    <UiSkeleton class="col-span-2 mb-[7px] mt-2.5 h-[10px] w-[180px] md:h-[15px]" />
+    <UiSkeleton class="col-span-2 h-[10px] w-[70px] md:h-[15px]" />
+
     <div
-      class="col-span-2 mt-[7px] flex flex-col gap-[5px] md:mt-[20px] md:gap-[10px] 4xl:mt-[30px] 4xl:gap-[15px]"
+      class="col-span-2 mt-[15px] flex flex-col gap-[5px] md:mt-[20px] md:gap-[10px] 4xl:mt-[30px] 4xl:gap-[15px]"
     >
       <div class="flex gap-[7px]">
         <UiSkeleton class="h-[10px] w-[60px] md:h-[15px] 4xl:h-[18px]" />
