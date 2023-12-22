5abd344f700ada6881af422b71c604867526a23c
Kuzmenko Andrey <uncortvpro@gmail.com>
2023-12-22 15:00:45 +0100

    revert useapi

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -1,10 +1,15 @@
-export function useApiFetch<T>(url: string, options: any = {}) {
+import type { UseFetchOptions } from 'nuxt/app';
+
+export function useApiFetch<T>(url: string, options: UseFetchOptions<T> = {}) {
   let headers: any = {};
 
   const token = useCookie('XSRF-TOKEN');
 
   if (token.value) {
-    headers['X-XSRF-TOKEN'] = token.value as string;
+    // Only set the X-XSRF-TOKEN header if it doesn't exist in the headers already
+    if (!headers['X-XSRF-TOKEN']) {
+      headers['X-XSRF-TOKEN'] = token.value as string;
+    }
   }
 
   if (process.server) {
@@ -14,8 +19,9 @@ export function useApiFetch<T>(url: string, options: any = {}) {
     };
   }
 
-  return $fetch<T>(url, {
+  return useFetch(url, {
     credentials: 'include',
+    watch: false,
     ...options,
     headers: {
       ...headers,
