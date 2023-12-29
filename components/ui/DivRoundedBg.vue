014b96e7cd78f65324d0cc4d7e39ef29f6026aaf
Bohdan2241 <bohdanyezhlov@gmail.com>
2023-12-29 13:35:35 +0100

    fix conflict, chat area

--- components/ui/DivRoundedBg.vue
+++ components/ui/DivRoundedBg.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="rounded-[10px] bg-white px-[15px] py-[20px] md:rounded-[20px] md:px-[25px] md:py-[23px] xl:max-h-[552px] xl:rounded-[30px] xl:px-[30px] xl:py-[27px]"
+    class="rounded-[10px] bg-white px-[15px] py-[20px] md:rounded-[20px] md:px-[25px] md:py-[23px] xl:h-full xl:max-h-[550px] xl:rounded-[30px] xl:px-[30px] xl:py-[27px]"
   >
     <slot />
   </div>
--- pages/profile/index/message.vue
+++ pages/profile/index/message.vue
@@ -10,10 +10,6 @@
   const loadingChats = ref(true);
   const loadingChat = ref(false);
 
-  const getBusinessFromCookies = () => {
-    chatSwitch.value = business.value || 'buying';
-  };
-
   const changeCurrentId = (id: number) => {
     currentIdChat.value = id;
   };
@@ -87,15 +83,13 @@
           block: !currentIdChat,
         })
       "
-      @mouseover="hideOverflow"
-      @mouseout="showOverflow"
     >
       <CommonTitleProfilePage class="mb-[20px] md:mb-[30px] xl:mb-[40px]">
         {{ $t('profile.message.my_messages') }}
       </CommonTitleProfilePage>
-      <CommonBusinessSwitcher v-if="isExporter" v-model="chatSwitch" :currentValue="chatSwitch" />
+
       <NuxtScrollbar
-        class="mx-[-20px] mt-[20px] flex h-[50vh] flex-col overflow-scroll !overflow-x-hidden px-[20px] md:mt-[27px] xl:mt-[41px]"
+        class="mx-[-20px] mt-[20px] flex h-[50vh] flex-col overflow-scroll !overflow-x-hidden px-[20px] md:mt-[27px] xl:mt-[41px] xl:h-[420px]"
         tag="div"
       >
         <div v-if="loadingChats">
@@ -115,33 +109,6 @@
           :key="chat.id"
           :chat="chat"
           :active="chat.id === currentIdChat"
-          :chatType="chatSwitch"
-          @click="changeCurrentId(chat.id)"
-        />
-      </NuxtScrollbar>
-      <NuxtScrollbar
-        v-if="chatSwitch === 'buying'"
-        class="mx-[-20px] mt-[20px] flex h-[50vh] max-h-[569px] flex-col overflow-scroll !overflow-x-hidden px-[20px] md:mt-[27px] xl:mt-[41px]"
-        tag="div"
-      >
-        <div v-if="loadingChats">
-          <UiSkeletonChatItem
-            v-for="item in 6"
-            :key="item"
-            class="px-[15px] py-[10px] md:py-[14px]"
-          />
-        </div>
-
-        <UiAlertProfileEmpty v-if="!loadingChats && buyChats.length === 0">
-          {{ $t('profile.message.you_have_no_chats_this_category') }}
-        </UiAlertProfileEmpty>
-
-        <PagesButtonChatItem
-          v-for="chat in buyChats"
-          :key="chat.id"
-          :active="chat.id === currentIdChat"
-          :chatType="chatSwitch"
-          :chat="chat"
           @click="changeCurrentId(chat.id)"
         />
       </NuxtScrollbar>
@@ -157,8 +124,6 @@
       :loading="loadingChat"
       :chat="currentChat"
       @changeCurrentIdChat="changeCurrentId"
-      @mouseover="hideOverflow"
-      @mouseout="showOverflow"
     />
   </div>
 </template>
