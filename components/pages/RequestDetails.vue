e010a03620ed8bd53f7f45dc77750de3d8857c78
Roman <romazelenskiy2000@gmail.com>
2024-01-05 10:56:26 +0200

    fix

--- components/pages/RequestDetails.vue
+++ components/pages/RequestDetails.vue
@@ -8,21 +8,30 @@
 
   const items = computed(() => [
     {
+      id: 1,
       label: t('labels.more_information_about_request'),
       content: TabTemplateTest,
       data: props.request?.description,
     },
     {
+      id: 2,
       label: t('labels.more_about_importer'),
       content: TabTemplateTest,
       data: props.request?.company?.description,
     },
   ]);
+
+  const hideItems = computed(() => {
+    if (props.request?.company) {
+      return items.value;
+    }
+    return items.value.filter((el) => el.id !== 2);
+  });
 </script>
 
 <template>
   <UiTabs
-    :items="items"
+    :items="hideItems"
     :ui="{
       wrapper: 'overflow-hidden mb-[25px] text-[12px] md:mb-[100px] md:text-[16px]',
     }"
