f937616451624fa9b9374597c573e55b5c2d1602
Bohdan2241 <bohdanyezhlov@gmail.com>
2024-01-05 17:34:19 +0100

    add styled pricingCards

--- pages/subscriptions.vue
+++ pages/subscriptions.vue
@@ -25,9 +25,13 @@
         v-if="loading"
         class="grid grid-cols-1 gap-[10px] sm:grid-cols-2 md:grid-cols-4 md:gap-[20px] xl:gap-[30px]"
       >
-        <SkeletonPricingCard v-for="item in [1, 2, 3, 4]"></SkeletonPricingCard>
+        <SkeletonPricingCard v-for="i in 4" :key="i" />
       </div>
-      <div v-if="!loading" class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
+
+      <div
+        v-if="!loading"
+        class="grid auto-rows-fr grid-cols-1 gap-y-[15px] md:grid-cols-2 md:gap-6 md:gap-x-[12px] md:gap-y-[20px] lg:grid-cols-3 2xl:grid-cols-4 2xl:gap-x-[30px] 2xl:gap-y-[40px]"
+      >
         <CommonPricingCard
           v-for="subscription in subscriptions"
           :key="subscription.id"
